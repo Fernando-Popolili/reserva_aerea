058e9fd1686aee48de10acaa54be71b53c31f23d
Fernando-Popolili <fpopolili@gmail.com>
2024-05-09 00:45:51 -0300

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import View.InicioConsulta_GUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+         new InicioConsulta_GUI().setVisible(true);
+         
+    }
+    
+}
